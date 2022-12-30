ecccd782dc4a94ab77f8c362ff3b191d46ebdb52
claudomiro <claudomiromonteiro@gmail.com>
2022-12-30 16:45:06 -0300

    update vercel.json 3

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(cors())
 
 const prisma = new PrismaClient({})
 
-app.get('/', async (request, response) => {
+export default app.get('/', async (request, response) => {
   const expense = await prisma.expenses.findMany()
 
   return response.json(expense)
